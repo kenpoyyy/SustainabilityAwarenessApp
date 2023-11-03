c097f28ec3c6d097cce4ec76727ef36b939ce314
kenpoyyy <kenpatrick.borja@gmail.com>
2023-11-03 15:19:37 +0800

    ----- TODO ----- - Texts inside TextView to be placed in JAVA (see Article1.java file for reference). - Linking of pages.
    ----- NOT DONE -----
    Shopping Guide, Login, Register, and Events pages not done (Requires Firebase).

---
+++ app/src/main/java/com/example/sustainabilityapp/ShoppingGuidePage2.java
@@ -0,0 +1,14 @@
+package com.example.sustainabilityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingGuidePage2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_guide_page2);
+    }
+}
