81e06729dd19db1c04a281fe822318edc850c4f2
teemothebest <ma1510405@126.com>
2023-11-16 08:45:17 +0800

    sign in with email and password done
    need to do next : edit profile page

--- app/src/main/java/com/example/sustainabilityapp/RegisterPage.java
+++ app/src/main/java/com/example/sustainabilityapp/RegisterPage.java
@@ -1,19 +1,17 @@
 package com.example.sustainabilityapp;
 
-import static androidx.constraintlayout.widget.Constraints.TAG;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,40 +19,78 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity {
 
     /* Variable Declaration */
-    private ImageView backbtn_regpage;
+    ImageView backbtn_regpage;
     EditText editUserName,editPassword,editEmail,editPhone,editCity;
     Button SignUp;
-    private FirebaseAuth mAuth;
+    //TextView alreadyAccount;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
-    //test
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Toast.makeText(RegisterPage.this,"User Already logged in",Toast.LENGTH_SHORT).show();
-        }
-    }
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+        getSupportActionBar().hide();
 
         // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
         editUserName=findViewById(R.id.editTextText6);
         editPassword=findViewById(R.id.editTextText7);
         editEmail=findViewById(R.id.editTextText8);
         editPhone=findViewById(R.id.editTextText9);
         editCity=findViewById(R.id.editTextText10);
         SignUp=findViewById(R.id.button);
+        // alreadyAccount=findViewById(R.id.alreadyAccount);
+        auth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+
+        //progress dialog set up
+        progressDialog=new ProgressDialog(RegisterPage.this);
+        progressDialog.setTitle("Creating your Account");
+        progressDialog.setMessage("Your account is creating");
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name=editUserName.toString();
+                String password=editPassword.toString();
+                String email=editEmail.toString();
+                String phone=editPhone.toString();
+                String city=editCity.toString();
+
+                auth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+
+                                    UserProfile profile=new UserProfile(name,email,password,phone,city);
+                                    String id=task.getResult().getUser().getUid();
+
+                                    database.getReference().child("Users").child(id).setValue(profile);
+                                    Intent intent=new Intent(RegisterPage.this,HomePage.class);
+                                    startActivity(intent);
+
+                                }
+                                else{
+
+                                    Toast.makeText(RegisterPage.this, task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+            }
+        });
+
 
         /* Assignment Statement */
         backbtn_regpage = (android.widget.ImageView) findViewById(R.id.backbtn_regpage);
@@ -67,7 +103,8 @@ public class RegisterPage extends AppCompatActivity {
             }
         });
 
-        SignUp.setOnClickListener(new View.OnClickListener() {
+
+        /*SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -120,9 +157,7 @@ public class RegisterPage extends AppCompatActivity {
                     }
                 });
             }
-        });
-
-
+        });*/
     }
 
 
