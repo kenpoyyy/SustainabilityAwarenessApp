81cb71af4f262d98b7bb9b2f84988aabee179ca7
kenpoyyy <kenpatrick.borja@gmail.com>
2023-11-23 23:59:27 +0800

    --- Done --- added 3 product for testing

--- app/src/main/java/com/example/sustainabilityapp/ShoppingGuide.java
+++ app/src/main/java/com/example/sustainabilityapp/ShoppingGuide.java
@@ -49,8 +49,8 @@ public class ShoppingGuide extends AppCompatActivity implements SelectListener{
         productInfo.add(new ProductInfo("Bamboo Spoon","$28.90","SustainableSG","Made from bamboo, a natural and renewable source, these eco-friendly and reusable spoons will bring you a step closer towards a more sustainable Singapore and a more environmentally responsible you!","Not only are these spoons made to last, they are wax-free and uncoated to keep your food away from unpleasant odours and harmful toxins. Dish out delicious meals for your loved ones while saving the environment, one stir at a time.",
                 "Wood Spoon","$19.80"," Bamboo spoons are a sustainable choice, as bamboo grows rapidly and requires minimal resources. This makes them an eco-friendly alternative to plastic or metal utensils. Their biodegradability also ensures a lower environmental impact, contributing positively to efforts in reducing waste and promoting sustainable living practices.","https://sustainablesg.com/products/across-the-land-bamboo-spoons","https://sustainablesg.com/products/across-the-land-bamboo-spoons",R.drawable.bamboon_spoon,R.drawable.wood_spoon,4));
         productInfo.add(new ProductInfo("Food Warp","$28.90","SustainableSG","Made with beeswax, tree resin and plant oils, SustainableSG’s Beeswax Wraps are washable, reusable and compostable. Keep your tasty leftovers fresh without contributing to plastic wastage by using eco-friendly food wraps instead.","Reusable and cost-saving, these Beeswax life-savers are also free of harmful toxins or chemicals which might seep into your food. Committed to the sustainability of the world, we provide quality products that are safe for the customer and the environment.",
-                "Plastic Food Wrap","$18.90","Compact and efficient, this food wrap preserves freshness remarkably well. Easy to use, eco-friendly, and durable. Great for everyday use.","https://sustainablesg.com/products/mama-shop-reusable-food-wrap","https://sustainablesg.com/products/mama-shop-reusable-food-wrap",R.drawable.food_wrap,R.drawable.food_wrap2,4));
-        productInfo.add(new ProductInfo("Borosilicate Glass Bottle","$44.90","Your Sustainable Store","Made of hand blown borosilicate glass, these reusable bottles are lightweight and suitable for both hot and cold beverages. Your perfect companion to stay hydrated on-the-go! The colourful silicon sleeve provides insulation as well as a firm grip of the bottle. Every SoL bottle comes with a waterproof hemp carry bag and a cleaning brush. ","Bottle is made of hand blown borosilicate glass\n" +
+                "Food Wrap","$18.90","Compact and efficient, this food wrap preserves freshness remarkably well. Easy to use, eco-friendly, and durable. Great for everyday use.","https://sustainablesg.com/products/mama-shop-reusable-food-wrap","https://sustainablesg.com/products/mama-shop-reusable-food-wrap",R.drawable.food_wrap,R.drawable.food_wrap2,4));
+        productInfo.add(new ProductInfo("Borosilicate Bottle","$44.90","SustainableSG","Made of hand blown borosilicate glass, these reusable bottles are lightweight and suitable for both hot and cold beverages. Your perfect companion to stay hydrated on-the-go! The colourful silicon sleeve provides insulation as well as a firm grip of the bottle. Every SoL bottle comes with a waterproof hemp carry bag and a cleaning brush. ","Bottle is made of hand blown borosilicate glass\n" +
                 "Colourful sleeve is made from silicon\n" +
                 "Exterior of cap is bamboo, underside is steel\n" +
                 "BPA and chemical free\n" +
@@ -62,12 +62,12 @@ public class ShoppingGuide extends AppCompatActivity implements SelectListener{
 
         recyclerView=findViewById(R.id.product_list);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 1, RecyclerView.VERTICAL, false));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2, RecyclerView.VERTICAL, false));
         productItems=new ArrayList<>();
         //add in items
         productItems.add(new ProductItem("Bamboo Spoons","SustainableSG","$28.90",R.drawable.bamboon_spoon));
-        productItems.add(new ProductItem("Bamboo Spoons","SustainableSG","$28.90",R.drawable.food_wrap));
-        productItems.add(new ProductItem("Borosilicate Glass Bottle","Your Sustainable Store","$44.90",R.drawable.borosilicate_glass_bottle));
+        productItems.add(new ProductItem("Food Wrap","SustainableSG","$28.90",R.drawable.food_wrap));
+        productItems.add(new ProductItem("Borosilicate Bottle","SustainableSG","$44.90",R.drawable.borosilicate_glass_bottle));
         //few more to go
 
 
